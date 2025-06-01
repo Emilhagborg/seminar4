9930d16d4f1b09e06ffa23dfa7adf03eebc6b499
Emilhagborg <Emil.hagborg@telia.com>
2025-06-01 20:08:44 +0200

    Update InvalidAgeException.java

--- src/main/java/integration/InvalidAgeException.java
+++ src/main/java/integration/InvalidAgeException.java
@@ -7,8 +7,16 @@ package integration;
 /**
  *
  * @author emilhagborg
+ * Representerar ett undantag som kastas när en ålder som är ogiltig.
+ * Detta kan inträffa om en ålder är negativ, noll, eller om personen är över 130(orimligt).
  */
 public class InvalidAgeException extends Exception {
+    /**
+     * Skapar ett nytt undantag som representerar ogiltig ålder.
+     * Meddelandet förklarar varför åldern anses vara ogiltig.
+     *
+     * @param message Det meddelande som beskriver orsaken till undantaget.
+     */
     public InvalidAgeException(String message) {
         super(message);
     }
