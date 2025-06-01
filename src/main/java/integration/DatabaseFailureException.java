575580b16330b4347048c97e18cf1e3334e1c4fb
Emilhagborg <Emil.hagborg@telia.com>
2025-06-01 20:07:21 +0200

    Update DatabaseFailureException.java

--- src/main/java/integration/DatabaseFailureException.java
+++ src/main/java/integration/DatabaseFailureException.java
@@ -3,12 +3,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package integration;
-
 /**
- *
- * @author emilhagborg
+ * Ett undantag som throws när det inte går att komma åt de externa systemen,
+ * till exempel på grund av ett simulerat databasfel.
+ * 
+ * Detta undantag används för en situation där databasservern inte är igång
+ * eller inte kan nås.
+ * 
+ * Detta är ett okontrollerat undantag  eftersom felet inte kan 
+ * hanteras på ett meningsfullt sätt där det inträffar.
+ * 
+ * @author Emil Hagborg
  */
 public class DatabaseFailureException extends RuntimeException {
+    /**
+     * Skapar ett nytt undantag som representerar ett databasfel.
+     * 
+     * @param message Ett meddelande som beskriver orsaken till felet.
+     */
     public DatabaseFailureException(String message) {
         super(message);
     }
