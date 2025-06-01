9b0bce31c147f0ab12d8f67fa20321205a31613f
Emilhagborg <Emil.hagborg@telia.com>
2025-06-01 20:06:40 +0200

    Update InvalidQuantityException.java

--- src/main/java/Module/InvalidQuantityException.java
+++ src/main/java/Module/InvalidQuantityException.java
@@ -5,10 +5,17 @@
 package Module;
 
 /**
+ * Representerar ett undantag som kastas när antalet handlade varor är ogiltig.
+ * Detta undantag visar att antalet är noll eller negativ,
  *
  * @author emilhagborg
  */
 public class InvalidQuantityException extends Exception {
+    /**
+     * Skapar ett nytt undantag som representerar ett antal fel.
+     * 
+     * @param message Ett meddelande som beskriver orsaken till felet.
+     */
     public InvalidQuantityException(String message) {
         super(message);
     }
