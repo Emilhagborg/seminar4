0edb470b2ab78882fe7765924d7ea089cbbacb4c
Emilhagborg <Emil.hagborg@telia.com>
2025-06-01 20:09:14 +0200

    Update ItemNotFoundException.java

--- src/main/java/integration/ItemNotFoundException.java
+++ src/main/java/integration/ItemNotFoundException.java
@@ -7,9 +7,17 @@ package integration;
 /**
  *
  * @author emilhagborg
+ * Representerar ett undantag som kastas när en vara inte kunde hittas.
+ * Detta undantag visar att en sökning efter en vara i invemtorysytem misslyckades
+ * eftersom inget objekt med det angivna ID matchade.
  */
 public class ItemNotFoundException extends Exception {
-    
+    /**
+     * Skapar ett nytt undantag som representerar att varan gick inte att komma åt.
+     * Meddelandet förklarar varför Id anses vara ogiltig.
+     *
+     * @param itemID är det id som inte funkade.
+     */
     public ItemNotFoundException(String itemID) {
         super("varan med detta ID " + itemID + " hittades inte.");
     }
